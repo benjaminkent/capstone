b843265412c93ed11aa4a84d9fa999b4171901f4
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-10-24 16:32:29 -0400

    started

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h1>ManDate</h1>
+          <i class="fas fa-user" />
+        </header>
+      </>
+    )
+  }
+}
+
+export default Header
