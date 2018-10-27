9be34c7b66e43f396a1d5bc03ed1663b158b47a8
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-27 02:14:01 -0400

    started image. added class.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
 import Header from './SubComponents/Header'
+import MainImage from './SubComponents/MainImage'
 
 class Home extends Component {
   render() {
     return (
       <>
         <Header />
+        <MainImage />
       </>
     )
   }
---
+++ src/Components/SubComponents/MainImage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MainImage extends Component {
+  render() {
+    return (
+      <>
+        <section className="user-image" />
+      </>
+    )
+  }
+}
+
+export default MainImage
