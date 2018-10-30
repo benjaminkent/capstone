85bc79ea02744c0efff01079fdd1cdcb4286db0e
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-29 20:50:37 -0400

    working on large layout. 2 sections complete

--- src/Components/SubComponents/MainPage.jsx
+++ src/Components/SubComponents/MainPage.jsx
@@ -5,7 +5,7 @@ class MainPage extends Component {
     return (
       <>
         <div className="main-page-content">
-          <section>
+          <section className="user-section">
             <div className="user-image-container">
               <div className="current-matches">
                 <h3>Budding Brolationships</h3>
@@ -15,13 +15,11 @@ class MainPage extends Component {
                 <p>90% Match</p>
               </div>
               <div className="image-container">
-                <i className="fas fa-arrow-left" />
                 <img
                   className="user-image"
                   src="./images/user-image.jpg"
                   alt="user"
                 />
-                <i className="fas fa-arrow-right" />
               </div>
               <div className="thumbs">
                 <i className="fas fa-thumbs-down" />
@@ -30,70 +28,70 @@ class MainPage extends Component {
             </div>
           </section>
 
-          <main className="about-section">
-            <section>
-              <div className="about-section-container">
-                <h2 className="about-user">About Ben</h2>
-                <div className="about-underline" />
-                <article className="about-questions">
-                  <p>What do you like to drink?</p>
-                  <p>Whiskey, Beer, Wine</p>
-                </article>
-                <article className="about-questions">
-                  <p>Do you smoke?</p>
-                  <p>Sometimes</p>
-                </article>
-                <article className="about-questions">
-                  <p>What do you smoke?</p>
-                  <p>Cigars</p>
-                </article>
-                <article className="about-questions">
-                  <p>How do you shave?</p>
-                  <p>Safety Razor</p>
-                </article>
-                <article className="about-questions">
-                  <p>Do you workout?</p>
-                  <p>Often</p>
-                </article>
-                <article className="about-questions">
-                  <p>Do you have kids?</p>
-                  <p>No</p>
-                </article>
-                <article className="about-questions">
-                  <p>Do you have pets?</p>
-                  <p>Dog</p>
-                </article>
-                <article className="about-questions">
-                  <p>Do you like guns?</p>
-                  <p>Yes</p>
-                </article>
-                <article className="about-questions">
-                  <p>Are you single?</p>
-                  <p>No</p>
-                </article>
-                <article className="about-questions">
-                  <p>Do you play video games?</p>
-                  <p>Sometimes</p>
-                </article>
-                <article className="about-questions">
-                  <p>Do you play board games?</p>
-                  <p>No</p>
-                </article>
-                <article className="about-questions">
-                  <p>Do you like sports?</p>
-                  <p>Yes</p>
-                </article>
-                <article className="about-questions">
-                  <p>Do you like outdoor activities?</p>
-                  <p>Yes</p>
-                </article>
-              </div>
-            </section>
+          <section className="about-section">
+            <div className="about-section-container">
+              <h2 className="about-user">About Ben</h2>
+              <div className="about-underline" />
+              <article className="about-questions">
+                <p>What do you like to drink?</p>
+                <p>Whiskey, Beer, Wine</p>
+              </article>
+              <article className="about-questions">
+                <p>Do you smoke?</p>
+                <p>Sometimes</p>
+              </article>
+              <article className="about-questions">
+                <p>What do you smoke?</p>
+                <p>Cigars</p>
+              </article>
+              <article className="about-questions">
+                <p>How do you shave?</p>
+                <p>Safety Razor</p>
+              </article>
+              <article className="about-questions">
+                <p>Do you workout?</p>
+                <p>Often</p>
+              </article>
+              <article className="about-questions">
+                <p>Do you have kids?</p>
+                <p>No</p>
+              </article>
+              <article className="about-questions">
+                <p>Do you have pets?</p>
+                <p>Dog</p>
+              </article>
+              <article className="about-questions">
+                <p>Do you like guns?</p>
+                <p>Yes</p>
+              </article>
+              <article className="about-questions">
+                <p>Are you single?</p>
+                <p>No</p>
+              </article>
+              <article className="about-questions">
+                <p>Do you play video games?</p>
+                <p>Sometimes</p>
+              </article>
+              <article className="about-questions">
+                <p>Do you play board games?</p>
+                <p>No</p>
+              </article>
+              <article className="about-questions">
+                <p>Do you like sports?</p>
+                <p>Yes</p>
+              </article>
+              <article className="about-questions">
+                <p>Do you like outdoor activities?</p>
+                <p>Yes</p>
+              </article>
+            </div>
+          </section>
 
-            <section>
-              <div className="meme-section-container">
-                <h2 className="about-user">Sense of Humor</h2>
-                <div className="about-underline" />
+          <section className="humor-section">
+            <div className="meme-section-container">
+              <h2 className="about-user">Sense of Humor</h2>
+              <div className="about-underline" />
+              <div className="meme-image-container">
                 <img className="meme" src="./images/vampire.jpg" alt="meme" />
                 <img
                   className="meme"
@@ -112,8 +110,8 @@ class MainPage extends Component {
                 />
                 <img className="meme" src="./images/break-up.jpg" alt="meme" />
               </div>
-            </section>
-          </main>
+            </div>
+          </section>
         </div>
       </>
     )
