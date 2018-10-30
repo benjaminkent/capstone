e8f68435e6212bb0f98844fe5c4dc0db6ff6facd
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-29 21:22:16 -0400

    reorganized components structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 
 import './App.css'
+
 import Home from './Components/Home'
+import Header from './Components/SubComponents/Header'
+import Footer from './Components/SubComponents/Footer'
 
 class App extends Component {
   render() {
     return (
       <>
+        <Header />
         <Home />
+        <Footer />
       </>
     )
   }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
 
-import Header from './SubComponents/Header'
 import MainPage from './SubComponents/MainPage'
-import Footer from './SubComponents/Footer'
 
 class Home extends Component {
   render() {
     return (
       <>
-        <Header />
         <MainPage />
-        <Footer />
       </>
     )
   }
