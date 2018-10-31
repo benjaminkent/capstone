3d1a0c2b4dfdd149e67bb1ce7ef7972407b97d90
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-31 18:48:23 -0400

    working on about page

--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -3,11 +3,19 @@ import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 class AboutPage extends Component {
   render() {
+    let imgURL = './images/wolf-logo.png'
     return (
       <>
         <section>
           <h1 className="about-page-statement">About Find a Bro</h1>
-          <main>
+          <main
+            className="about-page-main"
+            style={{
+              backgroundImage: `url(${imgURL})`,
+              paddingBottom: '20rem',
+              backgroundSize: 'cover'
+            }}
+          >
             <h2>jibber jabber opening line</h2>
           </main>
         </section>
