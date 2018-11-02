ed79f3541463be71f893cfc2dd7fbc31bfd806ea
Benjamin JEHL <benjaminjehl@Benjamins-MacBook-Pro.local>
2018-11-02 13:24:27 -0400

    about page background complete

--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -3,17 +3,17 @@ import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 class AboutPage extends Component {
   render() {
-    let imgURL = './images/wolf-logo.png'
+    let imgURL = './images/wolf-logo-light-yellow.png'
     return (
       <>
         <section>
-          <h1 className="about-page-statement">About Find a Bro</h1>
           <div
             className="about-page-main"
             style={{
               backgroundImage: `url(${imgURL})`
             }}
           >
+            <h1 className="about-page-statement">About Find a Bro</h1>
             <h2 className="about-main-content">jibber jabber opening line</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cum
